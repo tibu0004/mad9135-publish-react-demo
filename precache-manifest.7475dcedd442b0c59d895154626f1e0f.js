081c01aeb83703289a9e31fd74e9f27240964f9b
Joao Nabosne <tibu0004@algonquinlive.com>
2019-10-30 13:03:02 -0400

    Updates

---
+++ precache-manifest.7475dcedd442b0c59d895154626f1e0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448d00fd7f4c350e74190799e77fa35e",
+    "url": "/mad9135-publish-react-demo/index.html"
+  },
+  {
+    "revision": "8a9d79b0bcbdb6c95458",
+    "url": "/mad9135-publish-react-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8aaa1c2f2e51744a01f3",
+    "url": "/mad9135-publish-react-demo/static/js/2.d075b9b6.chunk.js"
+  },
+  {
+    "revision": "8a9d79b0bcbdb6c95458",
+    "url": "/mad9135-publish-react-demo/static/js/main.60915f90.chunk.js"
+  },
+  {
+    "revision": "516fc7bcbe24bbd71390",
+    "url": "/mad9135-publish-react-demo/static/js/runtime-main.e2cdb6e0.js"
+  }
+]);
